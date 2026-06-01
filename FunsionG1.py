da8429ee940fa61b26fb0b632187e9f1a5a9fe88
JPpittigotti <jppittigotti@gmail.com>
2026-06-01 11:32:08 -0500

    Armado y Inicio del parcial

---
+++ FunsionG1.py
@@ -0,0 +1,7 @@
+        # Estudiante 1: [Aquí su nombre y cedula]
+        # Desarrollar una función que reciba un número entero y determine si es par o impar.
+
+
+
+    # Estudiante 2: [Aquí su nombre y cedula]   
+        # Desarrollar una función que reciba dos números y retorne el mayor de ellos.
