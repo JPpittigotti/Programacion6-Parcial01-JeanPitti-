78aa965bf82d10b192e9e3f3bade32cb8875388d
JPpittigotti <jppittigotti@gmail.com>
2026-06-01 12:38:29 -0500

    parte 1 de la segunda funcion (JP)

---
+++ FunsionG2.py
@@ -0,0 +1,16 @@
+class FunsionG2:
+
+    def __init__(self):
+        pass
+
+    # Estudiante 1:
+        # Desarrollar una función que reciba una lista de números y calcule la suma total.
+
+    def sumTotal(self, x):
+        num = 0
+        for j in x:
+            sum = sum + x.index(j)
+        return "la suma de los numeros es: " + num
+
+    # Estudiante 2:
+        # Desarrollar una función que reciba una lista de números y calcule el promedio.
