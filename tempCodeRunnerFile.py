3b5a27090f5ef919689554c978b11524374af68b
JPpittigotti <jppittigotti@gmail.com>
2026-06-01 13:11:19 -0500

    basura

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-from FunsionG2 import FunsionG2
-# from FunsionG3 import FunsionG3
-# from FunsionG4 import FunsionG4
-# from FunsionG5 import FunsionG5
-# from FunsionG6 import FunsionG6
-# from FunsionG7 import FunsionG7
